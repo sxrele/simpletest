9dff0b1e3c9a7626d0aaad5bf60c925141343b4c
sxrele <34661180+sxrele@users.noreply.github.com>
2017-12-18 20:53:47 +0000

    hmm

--- simple-test.js
+++ simple-test.js
@@ -3,8 +3,10 @@ import AnotherSimpleComponent from './another-simple-component';
 class SimpleTest extends React.Component {
 	render() {
 		return(
+			<div>
 			<div>This is a test</div>
 			<AnotherSimpleComponent />
+			</div>
 		);
 	}
 }
